45f530f57d813fbe03a5123a371ae1edd1538531
Yudit-f <you@example.com>
2026-05-27 20:32:44 +0300

    forth commit

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -2,36 +2,42 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
- 
+import { Router } from '@angular/router'; 
 
 
 @Component({
   selector: 'app-login-page',
+  standalone: true,
   imports: [FormsModule, HttpClientModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
-  constructor(private http: HttpClient) {}
-  onSubmit(form: NgForm) {
-    const loginData = {
-    username: form.value.username,
-    password: form.value.password
-  };
+  constructor(private http: HttpClient, private router: Router) {}
 
-   this.http.post('http://localhost:3000/api/login', loginData)
-    .subscribe({
-      next: (res: any) => {
-        if (res.success) {
-          console.log('Login successful');
-        } else {
-          console.log('Invalid username or password');
-        }
-      },
-      error: err => {
-        console.error('Server error', err);
-      }
-    });
+     message = '';
+
+  onSubmit(form: NgForm) {
+  const username = form.value.username;
+  const password = form.value.password;
+
+
+   this.http.get<any[]>(
+    `http://localhost:3000/users?name=${username}&password=${password}`
+  )
+  
+  .subscribe(res => {
+    if (res.length > 0) {
+      console.log('Login OK');
+        this.message = 'Login successful ';
+        this.router.navigate(['/home']);
+    } else {
+      console.log('Wrong username or password');
+      this.message = 'Invalid username or password';
+    }
+  });
+}
+  }
 
   
 
@@ -39,4 +45,4 @@ export class LoginPage {
 
 
 
-}}
+
