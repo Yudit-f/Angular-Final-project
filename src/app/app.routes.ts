19094cbee890eb95807460fe72fed35c18e6b01a
saraspitzer <spitzer6518081@gmail.com>
2026-05-27 20:26:28 +0300

    third commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePage } from './pages/home-page/home-page';
+import { RegisterPage } from './pages/register-page/register-page';
+import { LoginPage } from './pages/login-page/login-page';
+import { AllTripsPage } from './pages/all-trips-page/all-trips-page';
+import { MyTripsPage } from './pages/my-trips-page/my-trips-page';
+export const routes: Routes = [
+    { path: 'home', component: HomePage },
+    { path: 'register', component: RegisterPage },
+    { path: 'login', component: LoginPage },
+    { path: 'trips', component: AllTripsPage },
+    { path: 'my-trips', component: MyTripsPage },
+    { path: '**', redirectTo: 'login' },
+];
