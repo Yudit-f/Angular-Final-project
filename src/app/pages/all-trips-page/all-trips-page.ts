b7efbc451d18c6c4493aa82fc0afa86b1eecdf3b
Yudit-f <you@example.com>
2026-06-09 20:39:26 +0300

    trying admin

--- src/app/pages/all-trips-page/all-trips-page.ts
+++ src/app/pages/all-trips-page/all-trips-page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TripsService } from '../../services/trips-servic';
 import { Trip } from '../../models/trip.model';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login-service';
 @Component({
   selector: 'app-all-trips-page',
   imports: [],
@@ -13,11 +14,16 @@ import { Router } from '@angular/router';
 
 export class AllTripsPage implements OnInit{
   trips: Trip[] = [];
-  constructor(private tripsService: TripsService,  private router: Router) {}
+  user: any = null;
+  
+  constructor(private tripsService: TripsService,  private router: Router, public LoginService: LoginService) {}
 
+  
   ngOnInit() {
   this.tripsService.getTrips().subscribe(trips => {
     this.trips = trips;
+
+      this.user = this.LoginService.getCurrentUser();
 
         
 
